0c9ac3a756a707327296ca97ad9ad8e049699c17
Jin Park <jinpark.han@gmail.com>
2019-03-31 12:49:33 +0100

    Improve sync script to use more variable and clearer outputs. Add githooks for jenkins.

--- sync-serv-to-me.sh
+++ sync-serv-to-me.sh
@@ -5,16 +5,17 @@ echo "SERVER (SEB) =======> ME"
 read -p "Are you sure? (y/n) " -n 1 -r
 echo    # (optional) move to a new line
 today=`date '+%Y_%m_%d__%H_%M_%S'`;
+TARGET='seb:/home/jin/sites/bodt.jinis.online'
 LOCAL_DEV='/Users/jin/Personal/bodt-cms-master'
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     echo "===============Backing up first================="
     cp db.sqlite3 backup/dev/db.sqlite3._$today
-    rsync -av $LOCAL_DEV/media $LOCAL_DEV/backup/dev
-    echo "================================================"
-    rsync -av jin@seb:/home/jin/sites/bodt.jinis.online/media $LOCAL_DEV
-    echo "================================================"
-    scp jin@seb:/home/jin/sites/bodt.jinis.online/db.sqlite3 db.sqlite3
+    rsync -av ${LOCAL_DEV}/media ${LOCAL_DEV}/backup/dev
+    echo "===============RSYNC============================"
+    rsync -av jin@${TARGET}/media ${LOCAL_DEV}
+    echo "===============Replacing Database==============="
+    scp jin@${TARGET}/db.sqlite3 db.sqlite3
     echo ""
     echo "================================================"
     echo "Media and database sync-ed (downloaded) \/ \/ \/"
