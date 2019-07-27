6298f4542b3c01543e749b2f7f8f8fe2ed4d6879
Jin Park <jinpark.han@gmail.com>
2019-07-27 21:33:22 +0100

    Add ordering, and featured to homepage

--- home/models.py
+++ home/models.py
@@ -51,7 +51,8 @@ class HomePage(Page):
         context['eventpages'] = eventpages
 
         # peoplepages = PeoplePage.objects.live().order_by('-first_published_at')[0:8]
-        peoplepages = PeoplePage.objects.live().order_by('?')[0:8]
+        # peoplepages = PeoplePage.objects.live().order_by('?')[0:8]
+        peoplepages = PeoplePage.objects.live().filter(featured=True).order_by('ordering_priority')[0:8]
         context['peoplepages'] = peoplepages
 
         # Same as news. Take 3 latest and make order from oldest to newest.
@@ -99,11 +100,24 @@ class PeoplePage(Page):
 
     name = models.CharField(max_length=60, blank=True)
     description = RichTextField()
+    featured = models.BooleanField(default=False, help_text='Shows on the homepage')
+    past = models.BooleanField(default=False)
+    ordering_priority = models.IntegerField(blank=True, null=True, default=5,
+                                            # validators=[MaxValueValidator(10), MinValueValidator(1)],
+                                            choices=[(i,i) for i in range(1, 11)],
+                                            ) # Between 1-10?
 
     content_panels = Page.content_panels + [
         FieldPanel('name'),
         FieldPanel('description', classname="full"),
         InlinePanel('people_images', label="People images"),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('featured', classname="col4"),
+                FieldPanel('ordering_priority', classname="col4"),
+                FieldPanel('past', classname="col4")
+            ]),
+        ], heading='Helpful Ordering'),
     ]
 
     def __str__(self):
