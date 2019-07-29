67bdda21254d3cfa563f970152e24d1c00c17f63
Jin Park <jinpark.han@gmail.com>
2019-07-29 23:02:51 +0100

    Move production settings to settings

--- bodt/settings/base.py
+++ bodt/settings/base.py
@@ -238,3 +238,17 @@ SENDGRID_API_KEY = config('SENDGRIND_API_PASS')
 # EMAIL_HOST_PASSWORD = config('SENDGRIND_API_ID')
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
+
+
+
+ENV_IS_FOR = config('ENV_IS_FOR')
+
+if ENV_IS_FOR == 'production':
+    DEBUG = False
+    # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+    SENDGRID_SANDBOX_MODE_IN_DEBUG=False
+    SENDGRID_ECHO_TO_STDOUT=True
+
+    GA_KEY_FILEPATH = 'wagtail-237316-364e043ba940.json'
+    GA_VIEW_ID = 'ga:191694086'
--- bodt/settings/production.py
+++ bodt/settings/production.py
@@ -1,14 +1,14 @@
-from .base import *
-from decouple import config
-
-ENV_IS_FOR = config('ENV_IS_FOR')
-
-if ENV_IS_FOR == 'production':
-    DEBUG = False
-    # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-    SENDGRID_SANDBOX_MODE_IN_DEBUG=False
-    SENDGRID_ECHO_TO_STDOUT=True
-
-    GA_KEY_FILEPATH = 'wagtail-237316-364e043ba940.json'
-    GA_VIEW_ID = 'ga:191694086'
+# from .base import *
+# from decouple import config
+#
+# ENV_IS_FOR = config('ENV_IS_FOR')
+#
+# if ENV_IS_FOR == 'production':
+#     DEBUG = False
+#     # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#
+#     SENDGRID_SANDBOX_MODE_IN_DEBUG=False
+#     SENDGRID_ECHO_TO_STDOUT=True
+#
+#     GA_KEY_FILEPATH = 'wagtail-237316-364e043ba940.json'
+#     GA_VIEW_ID = 'ga:191694086'
